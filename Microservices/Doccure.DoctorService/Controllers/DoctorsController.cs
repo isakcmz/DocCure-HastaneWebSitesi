e1994d09b9a8caa0b0fbfeafc98503a1dbb7628a
isakcmz <isafurkankacmaz@gmail.com>
2026-09-25 20:41:47 +0300

    Doctor için Controller'ın yazılması

---
+++ Microservices/Doccure.DoctorService/Controllers/DoctorsController.cs
@@ -0,0 +1,67 @@
+﻿using Doccure.DoctorService.Dtos.DoctorDtos;
+using Doccure.DoctorService.Services.DoctorServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doccure.DoctorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorsController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> DoctorList()
+        {
+            var values = await _doctorService.GetAllAsync();
+            return Ok(values);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor(CreateDoctorDto dto)
+        {
+            await _doctorService.CreateAsync(dto);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDoctor(string id)
+        {
+            await _doctorService.DeleteAsync(id);
+            return Ok("Silme işlemi başarılı");
+        }
+
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDoctor(UpdateDoctorDto dto)
+        {
+            await _doctorService.UpdateAsync(dto);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDoctorById(string id)
+        {
+            var value = await _doctorService.GetByIdAsync(id);
+            if (value == null)
+                return NotFound("Doktor bulunamadı");
+
+            return Ok(value);
+        }
+    }
+}
