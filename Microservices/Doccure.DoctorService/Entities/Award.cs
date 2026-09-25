ae448258673285113016e99375533487a6d15989
isakcmz <isafurkankacmaz@gmail.com>
2026-09-25 18:12:33 +0300

    Doctor Mikroservisini oluşturuma ve Entity'leri yazma

---
+++ Microservices/Doccure.DoctorService/Entities/Award.cs
@@ -0,0 +1,9 @@
+﻿namespace Doccure.DoctorService.Entities
+{
+    public class Award
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Description { get; set; }
+    }
+}
