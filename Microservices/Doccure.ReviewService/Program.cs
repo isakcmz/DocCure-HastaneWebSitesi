b52ef6137d6de1fc164ddedb6fc2d5d848ff669e
isakcmz <isafurkankacmaz@gmail.com>
2026-09-25 22:55:03 +0300

    Review için Mapping işlemi, Servislerin yazılması, Settings'lerin yazılması ve registration işlmeleri

--- Microservices/Doccure.ReviewService/Program.cs
+++ Microservices/Doccure.ReviewService/Program.cs
@@ -1,6 +1,21 @@
+using Doccure.ReviewService.Services.ReviewServices;
+using Doccure.ReviewService.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("DatabaseSettingsKey"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+    sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped<IReviewService, ReviewService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Microservices/Doccure.ReviewService/Services/ReviewServices/ReviewService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Doccure.ReviewService.Dtos.ReviewDtos;
+using Doccure.ReviewService.Entities;
+using Doccure.ReviewService.Settings;
+using MongoDB.Driver;
+
+namespace Doccure.ReviewService.Services.ReviewServices
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IMongoCollection<Review> _reviewCollection;
+        private readonly IMapper _mapper;
+
+        public ReviewService(IMapper mapper, IDatabaseSettings settings)
+        {
+            _mapper = mapper;
+
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _reviewCollection = database.GetCollection<Review>(settings.ReviewCollectionName);
+        }
+
+        public async Task CreateAsync(CreateReviewDto dto)
+        {
+            var entity = _mapper.Map<Review>(dto);
+            await _reviewCollection.InsertOneAsync(entity);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _reviewCollection.DeleteOneAsync(x => x.ReviewId == id);
+        }
+
+        public async Task<List<ResultReviewDto>> GetAllAsync()
+        {
+            var values = await _reviewCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultReviewDto>>(values);
+        }
+
+        public async Task<GetReviewByIdDto> GetByIdAsync(string id)
+        {
+            var value = await _reviewCollection.Find(x => x.ReviewId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetReviewByIdDto>(value);
+        }
+    }
+}
