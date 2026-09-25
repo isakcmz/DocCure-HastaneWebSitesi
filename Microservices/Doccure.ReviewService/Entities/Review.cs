37500696a2eab2fab456eba768c96de520030a58
isakcmz <isafurkankacmaz@gmail.com>
2026-09-25 21:44:26 +0300

    Review Mikroservisi oluşturma; Dto'lar ve Entity yazılması

---
+++ Microservices/Doccure.ReviewService/Entities/Review.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Doccure.ReviewService.Entities
+{
+    public class Review
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ReviewId { get; set; }
+        public string DoctorId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserImageUrl { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public bool Recommend { get; set; }
+        public int LikeCount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
