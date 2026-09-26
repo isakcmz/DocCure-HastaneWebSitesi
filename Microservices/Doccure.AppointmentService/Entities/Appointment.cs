8415345a52fedd55e3f351f43e28e99d3062c243
isakcmz <isafurkankacmaz@gmail.com>
2026-09-26 11:17:27 +0300

    Apoointment (Randevu) Mikroservisi oluşturma ve Entity'leri yazma

---
+++ Microservices/Doccure.AppointmentService/Entities/Appointment.cs
@@ -0,0 +1,13 @@
+﻿namespace Doccure.AppointmentService.Entities
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public string DoctorId { get; set; }     // Mongo'dan geliyor.
+        public string PatientId { get; set; }     // Identity'den gelecek.
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Microservices/Doccure.AppointmentService/Entities/AppointmentDetail.cs
@@ -0,0 +1,19 @@
+﻿namespace Doccure.AppointmentService.Entities
+{
+    public class AppointmentDetail
+    {
+        public int AppointmentDetailId { get; set; }
+        public int AppointmentId { get; set; }
+
+        public string Complaint { get; set; }
+        public string Notes { get; set; }
+
+        public string Diagnosis { get; set; }
+        public string Prescription { get; set; }
+
+        public bool IsFirstVisit { get; set; }
+        public DateTime? CompletedDate { get; set; }
+
+
+    }
+}
