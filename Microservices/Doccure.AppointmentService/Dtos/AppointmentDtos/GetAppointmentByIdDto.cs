394c11a10d3e9649ccf7a4fd13b136d36fcbf7ff
isakcmz <isafurkankacmaz@gmail.com>
2026-09-26 12:09:16 +0300

    Appointment için Dto'ların oluşturulması

---
+++ Microservices/Doccure.AppointmentService/Dtos/AppointmentDtos/GetAppointmentByIdDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Doccure.AppointmentService.Dtos.AppointmentDtos
+{
+    public class GetAppointmentByIdDto
+    {
+        public int AppointmentId { get; set; }
+        public string DoctorId { get; set; }
+        public string PatientId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Microservices/Doccure.AppointmentService/Services/AppointmentServices/IAppointmentService.cs
@@ -0,0 +1,15 @@
+﻿using Doccure.AppointmentService.Dtos.AppointmentDtos;
+using Doccure.AppointmentService.Entities;
+
+namespace Doccure.AppointmentService.Services.AppointmentServices
+{
+    public interface IAppointmentService
+    {
+        Task<List<ResultAppointmentDto>> GetAllAsync();
+        Task<GetAppointmentByIdDto> GetByIdAsync(int id);
+        
+        Task CreateAsync(CreateAppointmentDto dto);
+        Task UpdateAsync(UpdateAppointmentDto dto);
+        Task DeleteAsync(int id);
+    }
+}
