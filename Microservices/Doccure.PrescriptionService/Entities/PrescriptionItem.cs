c33fd5ee9686807a4b072d8a3331c48ccdc22dff
isakcmz <isafurkankacmaz@gmail.com>
2026-09-26 16:58:07 +0300

    Prescription (Reçete) Mikroservisinin oluşturulması, Entity'lerin yazılması, appsettings.json'da postresql bağlantısını oluşturma ve gerekli paketlerin kurulmasıı

---
+++ Microservices/Doccure.PrescriptionService/Entities/PrescriptionItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Doccure.PrescriptionService.Entities
+{
+    public class PrescriptionItem
+    {
+        public int PrescriptionItemId { get; set; }
+        public string MedicineName { get; set; }
+        public string Usage { get; set; }    // günde 2 kez
+        public string Duration { get; set; }      // 5 gün
+
+        public int PrescriptionId { get; set; }
+        public Prescription Prescription { get; set; }
+    }
+}
